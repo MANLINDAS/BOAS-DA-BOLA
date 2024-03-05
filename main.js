ad7dffcbbe664bedff39e2c200ca4428a9c8586e
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-03-05 08:52:47 -0300

    S

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 alert('OLA MARIA, BEM VINDA AO NOSSO SITE');
 let nomeUsuario = prompt('qual é seu nome');
-let anterior = document. querySelector ('#texto');
+let anterior = document.querySelector('#texto');
 
 anterior.textContent = nomeUsuario;
