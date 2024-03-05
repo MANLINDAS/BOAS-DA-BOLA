3f7634f33112a32b1343de62b6372c3a7eef8c27
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-03-05 09:13:25 -0300

    t

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 alert('OLA MARIA, BEM VINDA AO NOSSO SITE');
+let nomeUsuario = "";
 let anterior = document.querySelector('#texto');
 
-while(nomeUsuario == "" || nomeUsuario== null){ 
+while(nomeUsuario == ""){ 
     nomeUsuario = prompt('qual é seu nome');
+}
+
+if(nomeUsuario == null) {
+    anterior.textContent = 'usuario';
+} else{
+    anterior.textContent = nomeUsuario;
 }
 
 
