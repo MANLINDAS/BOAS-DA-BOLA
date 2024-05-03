5ac28c2cf81e4f7b9025ad7c67714565a7187de6
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-05-03 08:32:53 -0300

    j

--- const.js
+++ const.js
@@ -1,17 +1,18 @@
-const botoes = document.querySelectorAll('.objetivos'); 
+const botoes = document.querySelectorAll('.objetivos');
 const textos = document.querySelectorAll('.aba-conteudo');
 
 
 
 
 
-for(let x = 0; x< botoes.length; x++){
-    botoes[x].onclick = function(){
-        for(let y = 0; y<botoes.length; y++){
+for (let x = 0; x < botoes.length; x++) {
+    botoes[x].onclick = function () {
+        for (let y = 0; y < botoes.length; y++) {
             botoes[y].classList.remove('ativo');
             textos[y].classList.remove('ativa');
         }
         botoes[x].classList.add('ativo');
         textos[x].classList.add('ativa');
     }
-     
+}
+
