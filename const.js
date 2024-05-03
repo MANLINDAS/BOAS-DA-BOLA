2d7795c3ecedbb1c4e3f1751d3408c20d1d357cb
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-05-03 09:29:31 -0300

    fytdy

--- const.js
+++ const.js
@@ -16,3 +16,19 @@ for (let x = 0; x < botoes.length; x++) {
     }
 }
 
+const contadores  = document.querySelectorAll('.contador');
+
+let tempoAtual = new Date();
+let tempoObjetivo = new Date('2024-04-28T00:00:00');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+let segundos = Math.floor(tempoFinal/ 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
